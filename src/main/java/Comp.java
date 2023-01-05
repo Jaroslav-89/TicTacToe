0a9295791db4d9e6a06af1cfc70b8279a7c08832
Jaroslav-89 <jaroslav89@bk.ru>
2023-01-05 17:02:54 +0400

    Добавил возможность первого хода компьютера. Усложнил алгоритм поиска выигрышного хода для компьютера. Исправил ошибки в алгоритме компьютера.

--- src/main/java/Comp.java
+++ src/main/java/Comp.java
@@ -3,10 +3,17 @@ import java.util.ArrayList;
 public class Comp {
 
     String compSign;
+    String playerSign;
 
     Comp(String sign) {
         compSign = sign;
+        if (compSign.equals("X")) {
+            playerSign = "O";
+        } else {
+            playerSign = "X";
+        }
     }
+
 
     public ArrayList<Integer> compMove(String[][] field) {
         ArrayList<Integer> coordinatesList = new ArrayList<Integer>();
@@ -17,12 +24,71 @@ public class Comp {
 
         do {
 
+            //первым делом занимаем центр, если он не занят
             if (field[1][1].equals("*")) {
                 System.out.println("нашел свободное место в центре, записываю координаты");
                 coordinatesList.add(1);
                 coordinatesList.add(1);
                 return coordinatesList;
+            } else {
+                //если центр занят своим знаком, ищем на игровом поле еще свои ходы(если свой ход только один,
+                //то можно определить наилучший второй ход, в зависимости от хода игрока)
+                if (field[1][1].equals(compSign)) {
+                    int signCount = 0;//счетчик своих ходов на игровом поле
+                    for (int x = 0; x < 3; x++) {//
+                        System.out.println("ищу в строке " + x);
+                        for (int y = 0; y < 3; y++) {
+                            System.out.println("проверяю место " + y);
+                            if (field[x][y].equals(compSign)) {
+                                signCount++;
+                            }
+                        }
+                    }
+
+                    //определяем ход компьютера, если игрок поставил по центру слевa/справа/снизу/сверху
+                    if ((field[0][1].equals(playerSign) || field[2][1].equals(playerSign)) && signCount == 1) {
+                        //если игрок сходил сверху или снизу по центру и это второй ход, тогда ставим по центру слева
+                        coordinatesList.add(1);
+                        coordinatesList.add(0);
+                        return coordinatesList;
+                    } else {
+                        if ((field[1][0].equals(playerSign) || field[1][2].equals(playerSign)) && signCount == 1) {
+                            //если игрок сходил слева или справа по центру и это второй ход, тогда ставим по центру сверху
+                            coordinatesList.add(0);
+                            coordinatesList.add(1);
+                            return coordinatesList;
+                        }
+                    }
+
+                    //определяем ход компьютера, если игрок поставил по диагонали в любом углу
+                    if ((field[0][0].equals(playerSign) || field[2][2].equals(playerSign)) && signCount == 1) {
+                        //если игрок сходил в один из углов и это второй ход, тогда ставим в противоположный угол, если он свободен
+                        if (field[2][2].equals("*")) {
+                            coordinatesList.add(2);
+                            coordinatesList.add(2);
+                            return coordinatesList;
+                        } else {
+                            coordinatesList.add(0);
+                            coordinatesList.add(0);
+                            return coordinatesList;
+                        }
+                    } else {
+                        if ((field[2][0].equals(playerSign) || field[0][2].equals(playerSign)) && signCount == 1) {
+                            //если игрок сходил в один из углов и это второй ход, тогда ставим в противоположный угол, если он свободен
+                            if (field[2][0].equals("*")) {
+                                coordinatesList.add(2);
+                                coordinatesList.add(0);
+                                return coordinatesList;
+                            } else {
+                                coordinatesList.add(0);
+                                coordinatesList.add(2);
+                                return coordinatesList;
+                            }
+                        }
+                    }
+                }
             }
+
 
             for (int x = 0; x < 3; x++) {//сначала ищем на игровом поле два одинаковых знака по строкам
                 System.out.println("ищу в строке " + x);
@@ -97,24 +163,33 @@ public class Comp {
             System.out.println("ищу два символа по диагонали от 0 0 к 2 2");
             if ((field[0][0].equals("X") && field[1][1].equals("X")) ||
                     (field[0][0].equals("O") && field[1][1].equals("O"))) {//по диагонали от левого верха до правого низа
-                System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
-                coordinatesList.add(2);
-                coordinatesList.add(2);
-                return coordinatesList;
+                System.out.println("нашел два символа по диагонали");
+                if (field[2][2].equals("*")) {
+                    System.out.println("нашел свободное место, записываю координаты");
+                    coordinatesList.add(2);
+                    coordinatesList.add(2);
+                    return coordinatesList;
+                }
             } else {
                 if ((field[0][0].equals("X") && field[2][2].equals("X")) ||
                         (field[0][0].equals("O") && field[2][2].equals("O"))) {
-                    System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
-                    coordinatesList.add(1);
-                    coordinatesList.add(1);
-                    return coordinatesList;
+                    System.out.println("нашел два символа по диагонали");
+                    if (field[0][0].equals("*")) {
+                        System.out.println("нашел свободное место, записываю координаты");
+                        coordinatesList.add(1);
+                        coordinatesList.add(1);
+                        return coordinatesList;
+                    }
                 } else {
                     if ((field[1][1].equals("X") && field[2][2].equals("X")) ||
                             (field[1][1].equals("O") && field[2][2].equals("O"))) {
-                        System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
-                        coordinatesList.add(0);
-                        coordinatesList.add(0);
-                        return coordinatesList;
+                        System.out.println("нашел два символа по диагонали");
+                        if (field[0][0].equals("*")) {
+                            System.out.println("нашел свободное место, записываю координаты");
+                            coordinatesList.add(0);
+                            coordinatesList.add(0);
+                            return coordinatesList;
+                        }
                     }
                 }
             }
@@ -122,24 +197,33 @@ public class Comp {
             System.out.println("ищу два символа по диагонали от 2 0 к 0 2");
             if ((field[2][0].equals("X") && field[1][1].equals("X")) ||
                     (field[2][0].equals("O") && field[1][1].equals("O"))) {//по диагонали от правого верха до левого низа
-                System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
-                coordinatesList.add(0);
-                coordinatesList.add(2);
-                return coordinatesList;
+                System.out.println("нашел два символа по диагонали");
+                if (field[0][2].equals("*")) {
+                    System.out.println("нашел свободное место, записываю координаты");
+                    coordinatesList.add(0);
+                    coordinatesList.add(2);
+                    return coordinatesList;
+                }
             } else {
                 if ((field[2][0].equals("X") && field[0][2].equals("X")) ||
                         (field[2][0].equals("O") && field[0][2].equals("O"))) {
-                    System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
-                    coordinatesList.add(1);
-                    coordinatesList.add(1);
-                    return coordinatesList;
+                    System.out.println("нашел два символа по диагонали");
+                    if (field[1][1].equals("*")) {
+                        System.out.println("нашел свободное место, записываю координаты");
+                        coordinatesList.add(1);
+                        coordinatesList.add(1);
+                        return coordinatesList;
+                    }
                 } else {
                     if ((field[1][1].equals("X") && field[0][2].equals("X")) ||
                             (field[1][1].equals("O") && field[0][2].equals("O"))) {
-                        System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
-                        coordinatesList.add(2);
-                        coordinatesList.add(0);
-                        return coordinatesList;
+                        System.out.println("нашел два символа по диагонали");
+                        if (field[2][0].equals("*")) {
+                            System.out.println("нашел свободное место, записываю координаты");
+                            coordinatesList.add(2);
+                            coordinatesList.add(0);
+                            return coordinatesList;
+                        }
                     }
                 }
             }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
@@ -7,6 +8,8 @@ public class Main {
         PlayField playField = new PlayField();
         Player player = new Player();
         Comp comp;
+
+        ArrayList<Integer> correctMove;
 
         playField.createPlayField();
 
@@ -20,53 +23,59 @@ public class Main {
 
         System.out.println("Вы ставите: " + player.playerSign + "\nКомпьютер ставит: " + comp.compSign);
 
+        //можно выбрать, чтобы первым ходил компьютер
+        System.out.println("Если хотите, чтобы компьютер ходил перым, введите Comp");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next().trim();
+        if (answer.equalsIgnoreCase("Comp")) {
+            //получаем координаты в правильном формате(комп)
+            correctMove = comp.compMove(playField.field);
+
+            playField.viewChangePlayField(correctMove, comp.compSign);
+        }
+
         while (true) {
 
             System.out.println("Ваш ход! Для того чтобы поставить -" + player.playerSign +
                     "- введите координаты от 0 до 2 в формате: Y X (через пробел), где 0 0 это верхний левый угол");
 
-            ArrayList<Integer> correctMove;
 
-            while (true) {
+            //ход игрока
+            do {
                 //получаем координаты в правильном формате(игрок)
                 correctMove = player.checkInputMove();
                 //проверяем не занято ли поле, куда хочет сходить игрок
-                if (playField.checkCorrectMove(correctMove)) {
-                    break;
-                }
-            }
+            } while (!playField.checkCorrectMove(correctMove));
 
             playField.viewChangePlayField(correctMove, player.playerSign);
 
-            if (playField.checkWinGame(player.playerSign)){
+            if (playField.checkWinGame(player.playerSign)) {
                 System.out.println("Вы победили!");
                 break;
             }
 
-            if (!playField.checkFreeSpace()){
+            if (!playField.checkFreeSpace()) {
                 System.out.println("Ничья!");
                 break;
             }
 
-            while (true) {
+            //ход компьютера
+            do {
                 //получаем координаты в правильном формате(комп)
                 correctMove = comp.compMove(playField.field);
                 //проверяем не занято ли поле, куда хочет сходить компьютер
-                if (playField.checkCorrectMove(correctMove)) {
-                    break;
-                }
-            }
+            } while (!playField.checkCorrectMove(correctMove));
 
             System.out.println("Ход компьютера: " + correctMove.get(0) + " " + correctMove.get(1));
 
             playField.viewChangePlayField(correctMove, comp.compSign);
 
-            if (playField.checkWinGame(comp.compSign)){
+            if (playField.checkWinGame(comp.compSign)) {
                 System.out.println("Победил компьютер!");
                 break;
             }
 
-            if (!playField.checkFreeSpace()){
+            if (!playField.checkFreeSpace()) {
                 System.out.println("Ничья!");
                 break;
             }
