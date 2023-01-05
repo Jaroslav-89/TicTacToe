4af778f9be11f96c292a2cbc06f108ab2b82dd0c
Jaroslav-89 <jaroslav89@bk.ru>
2023-01-05 14:29:28 +0400

    Добавил простой ии.

--- src/main/java/Comp.java
+++ src/main/java/Comp.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Comp {
 
@@ -10,17 +9,143 @@ public class Comp {
     }
 
     public ArrayList<Integer> compMove(String[][] field) {
-        Random random = new Random();
         ArrayList<Integer> coordinatesList = new ArrayList<Integer>();
 
-        for (int x = 0; x < 3; x++) {
-            for (int y = 0; y < 3; y++) {
-                if (field[x][y] == "*") {
-                    coordinatesList.add(x);
-                    coordinatesList.add(y);
+        int count = 0; //счетчик для того, чтобы определить в первый раз проходится цикл или нет. В первое прохождение
+        //бесконечный цикл ищет два одинаковых знака в ряд, чтобы поставить любой третий в свободное место
+        //Любой знак подходит, тк этот ход будет подходить и для атаки и для защиты
+
+        do {
+
+            if (field[1][1].equals("*")) {
+                System.out.println("нашел свободное место в центре, записываю координаты");
+                coordinatesList.add(1);
+                coordinatesList.add(1);
+                return coordinatesList;
+            }
+
+            for (int x = 0; x < 3; x++) {//сначала ищем на игровом поле два одинаковых знака по строкам
+                System.out.println("ищу в строке " + x);
+                int sumAi = 0; //переменная в которой хранится сумма для оценки важности хода в определенное место
+                for (int y = 0; y < 3; y++) {
+                    System.out.println("проверяю место " + y);
+                    if (field[x][y].equals("X")) {
+                        sumAi = sumAi + 1;//если в одной строке будет стоять два Х, то общая сумма строки будет 2
+                    } else {
+                        if (field[x][y].equals("O")) {
+                            sumAi = sumAi + 10;//если в одной строке будет стоять два O, то общая сумма строки будет 20
+                            //разные цифры (1 и 10) использую для того, чтобы понять, что в строке
+                            //есть именно два одинаковых знака
+                        }
+                    }
+                }
+
+                if (sumAi == 2 || sumAi == 20) {//если нужная строка нашлась, находим в ней пустой элемент и записываем его координаты, чтобы туда сходить
+                    System.out.println("нашел строку с двумя одинаковыми символами");
+                    for (int y = 0; y < 3; y++) {
+                        System.out.println("ищу свободное место в этой строке");
+                        if (field[x][y].equals("*")) {
+                            System.out.println("нашел свободное место, записываю координаты");
+                            coordinatesList.add(x);
+                            coordinatesList.add(y);
+                            return coordinatesList;
+                        }
+                    }
+                } else {
+                    if ((sumAi >= 1) && count == 1) {
+                        System.out.println("нашел строку только с одним символом");
+                        for (int y = 0; y < 3; y++) {
+                            System.out.println("ищу свободное место в этой строке");
+                            if (field[x][y].equals("*")) {
+                                System.out.println("нашел свободное место, записываю координаты");
+                                coordinatesList.add(x);
+                                coordinatesList.add(y);
+                                return coordinatesList;
+                            }
+                        }
+                    }
                 }
             }
-        }
-        return coordinatesList;
+
+            for (int y = 0; y < 3; y++) {//ищем на игровом поле два одинаковых знака по столбцам
+                System.out.println("ищу в столбце " + y);
+                int sumAi = 0; //переменная в которой хранится сумма для оценки важности хода в определенное место
+                for (int x = 0; x < 3; x++) {
+                    System.out.println("проверяю место " + x);
+                    if (field[x][y].equals("X")) {
+                        sumAi = sumAi + 1;//если в одной строке будет стоять два Х, то общая сумма строки будет 2
+                    } else {
+                        if (field[x][y].equals("O")) {
+                            sumAi = sumAi + 10;//если в одной строке будет стоять два O, то общая сумма строки будет 20
+                        }
+                    }
+                }
+                if (sumAi == 2 || sumAi == 20) {
+                    System.out.println("нашел столбец с двумя одинаковыми символами");
+                    for (int x = 0; x < 3; x++) {
+                        System.out.println("ищу свободное место в этом столбце");
+                        if (field[x][y].equals("*")) {
+                            System.out.println("нашел свободное место, записываю координаты");
+                            coordinatesList.add(x);
+                            coordinatesList.add(y);
+                            return coordinatesList;
+                        }
+                    }
+                }
+            }
+
+            System.out.println("ищу два символа по диагонали от 0 0 к 2 2");
+            if ((field[0][0].equals("X") && field[1][1].equals("X")) ||
+                    (field[0][0].equals("O") && field[1][1].equals("O"))) {//по диагонали от левого верха до правого низа
+                System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
+                coordinatesList.add(2);
+                coordinatesList.add(2);
+                return coordinatesList;
+            } else {
+                if ((field[0][0].equals("X") && field[2][2].equals("X")) ||
+                        (field[0][0].equals("O") && field[2][2].equals("O"))) {
+                    System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
+                    coordinatesList.add(1);
+                    coordinatesList.add(1);
+                    return coordinatesList;
+                } else {
+                    if ((field[1][1].equals("X") && field[2][2].equals("X")) ||
+                            (field[1][1].equals("O") && field[2][2].equals("O"))) {
+                        System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
+                        coordinatesList.add(0);
+                        coordinatesList.add(0);
+                        return coordinatesList;
+                    }
+                }
+            }
+
+            System.out.println("ищу два символа по диагонали от 2 0 к 0 2");
+            if ((field[2][0].equals("X") && field[1][1].equals("X")) ||
+                    (field[2][0].equals("O") && field[1][1].equals("O"))) {//по диагонали от правого верха до левого низа
+                System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
+                coordinatesList.add(0);
+                coordinatesList.add(2);
+                return coordinatesList;
+            } else {
+                if ((field[2][0].equals("X") && field[0][2].equals("X")) ||
+                        (field[2][0].equals("O") && field[0][2].equals("O"))) {
+                    System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
+                    coordinatesList.add(1);
+                    coordinatesList.add(1);
+                    return coordinatesList;
+                } else {
+                    if ((field[1][1].equals("X") && field[0][2].equals("X")) ||
+                            (field[1][1].equals("O") && field[0][2].equals("O"))) {
+                        System.out.println("нашел два символа по диагонали\nнашел свободное место, записываю координаты");
+                        coordinatesList.add(2);
+                        coordinatesList.add(0);
+                        return coordinatesList;
+                    }
+                }
+            }
+            System.out.println("не нашел два символа, иду на второй круг");
+            count = count + 1;
+
+        } while (true);
     }
 }
